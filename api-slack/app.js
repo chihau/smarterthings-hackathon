2915fac0db448e9b47054142972164b41512ac4a
Niraj Desai <niraj@app.st>
2016-05-21 11:46:15 -0700

    slackbot sender, TODO: receiver

---
+++ api-slack/app.js
@@ -0,0 +1,30 @@
+var SlackBot = require('slackbots');
+ 
+// create a bot 
+var bot = new SlackBot({
+    token: 'xoxb-44793896054-XIdcvJTyqeAjfBLrLudZi6bH', // Add a bot https://my.slack.com/services/new/bot and put the token  
+    name: 'smarterthingsbot'
+});
+ 
+bot.on('start', function() {
+    // more information about additional params https://api.slack.com/methods/chat.postMessage
+    console.log("start");
+    var params = {
+        icon_emoji: ':cat:'
+    };
+    
+    // // define channel, where bot exist. You can adjust it there https://my.slack.com/services  
+    // bot.postMessageToChannel('general', 'meow!', params);
+    
+    // // define existing username instead of 'user_name' 
+    // bot.postMessageToUser('user_name', 'meow!', params); 
+    
+    // // define private group instead of 'private_group', where bot exist 
+    // bot.postMessageToGroup('private_group', 'meow!', params); 
+
+    
+    bot.on('message', function(data) {
+        // all ingoing events https://api.slack.com/rtm 
+        console.log(data);
+    });
+});
